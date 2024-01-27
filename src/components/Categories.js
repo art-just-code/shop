a8749fc13b8b0ede8b02caa5f47a962ef01f0931
Grebnik89 <116430298+Grebnik89@users.noreply.github.com>
2024-01-27 21:03:27 +0300

    before deploy

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -18,6 +18,9 @@ export default function Categories({ chooseCategory }) {
 
   return (
     <div className='categories' id='catalog'>
+=======
+    <div className='categories' id='categories'>
+>>>>>>> Stashed changes
       {categories.map(el => (
         <div key={el.key} onClick={() => chooseCategory(el.key)}>{el.name}</div>
       ))}
